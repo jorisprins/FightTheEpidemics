93d4fe3e251f34e56f2a8aea2500b09cdc945c19
goppuccino <goppuccino@gmail.com>
2015-12-08 15:53:07 +0100

    checkbox update

---
+++ app/src/main/java/nl/windesheim/fighttheepidemics/OtherChanceActivity.java
@@ -0,0 +1,26 @@
+package nl.windesheim.fighttheepidemics;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+
+//drop-down menu (spinner p.356)
+//submit button method
+//when it is clicked -> toast "your servey has been sent"
+//p.71
+
+
+public class OtherChanceActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_health_report_other_chance);
+    }
+
+    public void onlUnderstandClicked(View v){
+        finish();
+        //use intent flag
+    }
+}
