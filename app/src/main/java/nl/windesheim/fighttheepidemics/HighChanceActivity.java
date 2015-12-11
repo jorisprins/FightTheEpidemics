4b17b4f34ab8985cabc1d885640ca8d7bb7f5f99
gidie12 <gideonhessels@MacBook-Air-van-Gideon.local>
2015-12-11 11:19:10 +0100

    Deleting some not useful stuff

--- app/src/main/java/nl/windesheim/fighttheepidemics/HighChanceActivity.java
+++ app/src/main/java/nl/windesheim/fighttheepidemics/HighChanceActivity.java
@@ -1,6 +1,7 @@
 package nl.windesheim.fighttheepidemics;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -16,8 +17,6 @@ import android.widget.Toast;
 public class HighChanceActivity extends AppCompatActivity
 
 {
-    //int s = ((MyApplication) this.getApplication()).getChance();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,7 +27,7 @@ public class HighChanceActivity extends AppCompatActivity
         Intent HealthActivityIntent = new Intent(getApplicationContext(), HealthActivity.class);
         startActivity(HealthActivityIntent);
         //use intent flag
-        //((MyApplication) this.getApplication()).setChance(2);
+
     }
 
     public void onhReportClicked(View v) {
